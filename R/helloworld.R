13824d4158d08042a763e5111f2f7ab61f91ae60
Belinda Lombard <belindalombard@Belindas-MacBook-Pro.local>
2023-03-01 14:51:44 +0200

    project setup

--- R/helloworld.R
+++ R/helloworld.R
@@ -1,4 +1,12 @@
-# hello world function to print hello world to the screen
+#' Hello world
+#'
+#' @description Prints hello world
+#'
+#' @return prints hello world
+#' @export
+#'
+#' @examples
+#' helloworld()
 helloworld <- function() {
   print("hello world")
 }
